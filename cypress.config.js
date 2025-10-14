0dc71497a2602795eba4f3e58ff8aeb2ac7c8b93
rksamu <rksamu@users.noreply.github.com>
2025-10-14 12:41:35 -0300

    Updating Config Files

--- cypress.config.js
+++ cypress.config.js
@@ -5,7 +5,7 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    video: true,
+    video: false,
     reporter: 'mochawesome',
     reporterOptions: {
       reportDir: 'cypress/reports',
